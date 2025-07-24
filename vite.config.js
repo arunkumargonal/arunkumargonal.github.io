9728d1a103cc058ebfbeca0941228b93669a8274
arun <arunkumargonal@gmail.com>
2025-07-24 15:02:27 +0530

    package-lock.json: Update dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { ghPages } from "vite-plugin-gh-pages";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/arunkumargonal.github.io"
+  plugins: [react(), ghPages()],
+  base: "/arunkumargonal.github.io/"
 })
