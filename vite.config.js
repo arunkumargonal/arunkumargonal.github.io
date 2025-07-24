3663187c090fcb498ea568638a0bc0b21e4d1dcd
arun <arunkumargonal@gmail.com>
2025-07-24 15:40:48 +0530

    changes to config

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import { ghPages } from "vite-plugin-gh-pages";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), ghPages()],
-  base: "/arunkumargonal.github.io/"
+  base: "/"
 })
